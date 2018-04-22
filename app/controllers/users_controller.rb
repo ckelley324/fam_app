38edf343ca414617519cb0196b884b0166e171ab
Crystal Kelley <ckelley324@gmail.com>
2018-04-22 19:36:44 -0400

    fixed errors

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,6 +23,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       @user.send_activation_email
+      @user.send_new_user_email
       flash[:info] = "Please check your email to activate your account."
       redirect_to root_url
     else
